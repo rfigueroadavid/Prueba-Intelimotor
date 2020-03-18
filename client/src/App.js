730bb04c943990c02dc21f169a75295bb428b9f6
rfigueroadavid <41716652+rfigueroadavid@users.noreply.github.com>
2020-03-18 03:08:14 -0600

    .commit

---
+++ client/src/App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ButtonAppBar from './components/navbar';
+import { Container, Grid, Backdrop, CircularProgress } from '@material-ui/core';
+import Form from './components/form.js';
+import Axios from 'axios';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      publishing: "no-publicando", //publicando error exito
+      src: null,
+    }
+  }
+
+  render() {
+    const publicarAnuncio = (data) => {
+      let _this = this;
+      this.setState({ publishing: "publicando" })
+      let url = "http://localhost:8080/publicarAnuncio" 
+      let body = {
+        precio: data.precio,
+        descripcion: data.descripcion,
+        img: null
+      }
+      Axios.post(url, body)
+        .then(function (response) {
+          _this.setState({ publishing: "Anuncio Publicado", src: response.data.src })
+        })
+        .catch(function (error) {
+          _this.setState({ publishing: "Error al publicar anuncio" })
+        });
+    }
+
+    return (
+      <div className="App">
+        <ButtonAppBar />
+        {this.state.publishing != "publicando" ?
+
+          <Grid container style={{ paddingTop: "50px", paddingLeft: "10px", paddingRight: "10px" }}>
+            <Grid item xs={12} sm={6} >
+              <h4 style={{ textAlign: "left", marginLeft: "25px" }}>
+                Formulario
+      </h4>
+              <Form publicarAnuncio={publicarAnuncio} />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <h4 style={{ textAlign: "left", marginLeft: "25px" }}>
+                {!this.state.src ? "Anuncio sin publicar" :
+                  "Anuncio publicado correctamente"
+                }
+              </h4>
+
+              {!this.state.src ?
+                <p>
+                  Aqui se mostrará tu anuncio publicado
+                 </p>
+                : 
+                <img width="100%" src={this.state.src}></img>
+              }
+            </Grid>
+          </Grid>
+          :
+          <Backdrop open={this.state.publishing == "publicando" ? true : false} onClick={() => { }}>
+            <Grid container>
+              <Grid item xs={12}>
+                <h4 style={{ color: "#ffff" }}>
+                  Publicando Anuncio
+                          </h4>
+              </Grid>
+              <Grid item xs={12}>
+                <CircularProgress color="inherit" />
+              </Grid>
+            </Grid>
+          </Backdrop>
+
+        }
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ client/src/components/form.js/index.js
@@ -0,0 +1,96 @@
+
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import {  TextField, Grid, Icon } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            precio: null,
+            descripcion: "",
+            publishing: "sin-publicar",
+        }
+    }
+
+
+    render() {
+
+ 
+        return (
+                <Card >
+                    <CardContent>
+                        <Grid container>
+
+                            <Grid item xs={12}>
+                                <TextField
+                                    value={this.state.precio}
+                                    onChange={(e) => { this.setState({ precio: e.target.value }) }}
+                                    fullWidth type="number" id="precio" label="Precio" />
+                            </Grid>
+                        </Grid>
+
+                        <Grid container>
+                            <Grid item xs={12}>
+                                <TextField
+                                    fullWidth
+                                    placeholder="Coloca aqui la descripción de tu anuncion"
+                                    multiline
+                                    value={this.state.descripcion}
+                                    rows={2}
+                                    rowsMax={4}
+                                    onChange={(e) => { this.setState({ descripcion: e.target.value }) }}
+                                />
+                            </Grid>
+                        </Grid>
+                    </CardContent>
+                    <CardActions>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            endIcon={<Icon>send</Icon>}
+                            onClick={() => {
+                                this.props.publicarAnuncio({precio:this.state.precio, descripcion: this.state.descripcion});
+                            }}
+                            disabled={ this.state.precio!==null && this.state.descripcion!=='' ? false : true }
+                        >
+                            Publicar tu auto
+      </Button>
+                    </CardActions>
+                </Card>
+
+                
+               
+        );
+    }
+
+
+
+
+}
+
+
+
+
+export default Form
